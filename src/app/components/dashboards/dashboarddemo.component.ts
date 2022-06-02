4db879603cdd2b17d620048f98f7dd3b4256f7c6
Cagatay Civici <cagatay.civici@gmail.com>
2022-06-02 17:01:21 +0300

    Add topbar menu

--- src/app/components/dashboards/dashboarddemo.component.ts
+++ src/app/components/dashboards/dashboarddemo.component.ts
@@ -1,8 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import { BreadcrumbService } from 'src/app/service/app.breadcrumb.service';
 
 @Component({
     templateUrl: './dashboard.component.html'
 })
 export class DashboardDemoComponent {
+
+    constructor(private breadcrumbService: BreadcrumbService) {
+        this.breadcrumbService.setItems([
+            {label: 'E-Commerce Dashboard'}
+        ]);
+    }
 
 }
