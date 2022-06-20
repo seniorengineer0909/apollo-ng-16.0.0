7bf43650d2c5cdbe02c6f4b86cef355be14d5d3c
Cagatay Civici <cagatay.civici@gmail.com>
2022-06-20 18:25:20 +0300

    Reimplemented invoice as blocks

--- src/app/components/pages/invoice/app.invoice.component.ts
+++ src/app/components/pages/invoice/app.invoice.component.ts
@@ -1,19 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-    templateUrl: './app.invoice.component.html',
-    styleUrls: ['./app.invoice.component.scss']
+    templateUrl: './app.invoice.component.html'
 })
 export class AppInvoiceComponent {
 
-    print() {
-        window.print();
-    }
-
-    tableData = [
-        {name: 'Creative Design', date: '29 Jul 2021', hours: '80h', rate: '$40.00', amount: '$920.34'},
-        {name: 'Logo Design', date: '29 Jul 2021', hours: '10h', rate: '$20.00', amount: '$834.12'},
-        {name: 'Web Development', date: '29 Jul 2021', hours: '7h', rate: '$60.00', amount: '$340.20'},
-        {name: 'Logo Design', date: '29 Jul 2021', hours: '72h', rate: '$20.00', amount: '$834.12'}
-    ]
 }
