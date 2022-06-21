2a7c3f279602107e8c36a17d4660084ef7d71d84
yigitfindikli <yigitfindikli@gmail.com>
2022-06-21 14:56:48 +0300

    Gmap removed from contact

--- src/app/components/pages/contact/app.contact.module.ts
+++ src/app/components/pages/contact/app.contact.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppContactComponent } from './app.contact.component';
 import { AppContactRoutingModule } from './app.contact-routing.module';
-import { GMapModule } from 'primeng/gmap';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputTextareaModule } from 'primeng/inputtextarea';
@@ -11,7 +10,6 @@ import { InputTextareaModule } from 'primeng/inputtextarea';
   imports: [
     CommonModule,
     FormsModule,
-    GMapModule,
     ButtonModule,
     AppContactRoutingModule,
     InputTextModule,
