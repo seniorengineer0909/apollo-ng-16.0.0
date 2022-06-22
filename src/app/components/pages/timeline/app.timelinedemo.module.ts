496e606fb541416b55f6e37c628072d8c4313ad2
yigitfindikli <yigitfindikli@gmail.com>
2022-06-22 13:44:52 +0300

    Timeline added

--- src/app/components/pages/timeline/app.timelinedemo.module.ts
+++ src/app/components/pages/timeline/app.timelinedemo.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppTimelineDemoRoutingModule } from './app.timelinedemo-routing.module';
 import { AppTimelineDemoComponent } from './app.timelinedemo.component';
+import { TimelineModule } from 'primeng/timeline';
+import { ButtonModule } from 'primeng/button';
 @NgModule({
   imports: [
     CommonModule,
+    TimelineModule,
+    ButtonModule,
     AppTimelineDemoRoutingModule
   ],
   declarations: [AppTimelineDemoComponent]
