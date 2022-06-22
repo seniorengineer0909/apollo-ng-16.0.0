848f7ab56f4890147cd10f1ee09dd865c55cecce
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-06-22 09:57:56 +0300

    refactor

--- src/app/components/ecommerce/shopping-cart/app.shopping-cart.component.ts
+++ src/app/components/ecommerce/shopping-cart/app.shopping-cart.component.ts
@@ -12,4 +12,5 @@ export class AppShoppingCartComponent implements OnInit {
     ngOnInit(): void { }
 
     quantityOptions: SelectItem[] = [{ label: '1', value: 1 }, { label: '2', value: 2 }, { label: '3', value: 3 }, { label: '4', value: 4 }];
+    
 }
