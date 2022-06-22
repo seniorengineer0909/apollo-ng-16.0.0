2911823fe5350d0ae289a0f185dba2add27e5ede
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-06-22 10:56:43 +0300

    refactor

--- src/app/components/ecommerce-admin/productlist/app.productlist.module.ts
+++ src/app/components/ecommerce-admin/productlist/app.productlist.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { AppProductListRoutingModule } from './app.productlist-routing.module';
 import { AppProductListComponent } from './app.productlist.component';
 import { RippleModule } from 'primeng/ripple';
@@ -8,7 +7,6 @@ import { RippleModule } from 'primeng/ripple';
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
         AppProductListRoutingModule,
         RippleModule
     ],
