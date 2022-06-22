1121b0623ae87d68cf007202a1b2b7eccc6485ab
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-06-22 10:00:42 +0300

    Remove unnecessary imports

--- src/app/components/ecommerce/shopping-cart/app.shopping-cart.module.ts
+++ src/app/components/ecommerce/shopping-cart/app.shopping-cart.module.ts
@@ -1,28 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { AppShoppingCartComponent } from './app.shopping-cart.component';
-import { InputNumberModule } from 'primeng/inputnumber';
 import { ButtonModule } from 'primeng/button';
 import { DropdownModule } from 'primeng/dropdown';
-import { StyleClassModule } from 'primeng/styleclass';
 import { RippleModule } from 'primeng/ripple';
-import { DividerModule } from 'primeng/divider';
-import { SidebarModule } from 'primeng/sidebar';
 import { AppShoppingCartRoutingModule } from './app.shopping-cart-routing.module';
 
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
         AppShoppingCartRoutingModule,
-        InputNumberModule,
         ButtonModule,
-        SidebarModule,
-        StyleClassModule,
         RippleModule,
         DropdownModule,
-        DividerModule
     ],
     declarations: [AppShoppingCartComponent],
 })
