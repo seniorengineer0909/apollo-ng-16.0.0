3a93fe0bc382bc4214293432a2db6fb68061e405
Çetin <69278826+cetincakiroglu@users.noreply.github.com>
2022-06-22 13:55:34 +0300

    Merge branch 'master' of https://github.com/primefaces/apollo-ngx

--- src/app/components/pages/timeline/app.timelinedemo.component.ts
+++ src/app/components/pages/timeline/app.timelinedemo.component.ts
@@ -1,18 +1,55 @@
 import { Component } from '@angular/core';
+import { PrimeIcons } from 'primeng/api';
 
 @Component({
     templateUrl: './app.timelinedemo.component.html',
+    styles: [`
+        ::ng-deep {
+            .p-timeline-event-content,
+            .p-timeline-event-opposite {
+                line-height: 1;
+            }
+        }
+        
+        @media screen and (max-width: 960px) {
+            :host ::ng-deep {
+                .customized-timeline {
+                    .p-timeline-event:nth-child(even) {
+                        flex-direction: row !important;
+        
+                        .p-timeline-event-content {
+                            text-align: left !important;
+                        }
+                    }
+        
+                    .p-timeline-event-opposite {
+                        flex: 0;
+                    }
+        
+                    .p-card {
+                        margin-top: 1rem;
+                    }
+                }
+            }
+        }
+    `]
 })
 export class AppTimelineDemoComponent {
 
-    selectedIndex: number;
+    customEvents: any[];
 
-    timelineContent = [
-        {icon: 'pi pi-fw pi-book', title: 'Breakfast', info:'Aliquam diam vitae luctus tempor.'},
-        {icon: 'pi pi-fw pi-sun', title: 'Morning', info:'Placerat quis fames aenean cursus commodo morbi.'},
-        {icon: 'pi pi-fw pi-thumbs-up', title: 'Lunch', info:'Nec egestas mauris tincidunt tristique ut cum.'},
-        {icon: 'pi pi-fw pi-video', title: 'Afternoon', info:'Nec egestas mauris tincidunt tristique ut cum.'},
-        {icon: 'pi pi-fw pi-th-large', title: 'Supper', info:'Nec egestas mauris tincidunt tristique ut cum.'},
-        {icon: 'pi pi-fw pi-link', title: 'Evening', info:'Nec egestas mauris tincidunt tristique ut cum.'}
-    ];
+    ngOnInit() {
+        this.customEvents = [
+            {
+                status: 'Ordered',
+                date: '15/10/2020 10:30',
+                icon: PrimeIcons.SHOPPING_CART,
+                color: '#9C27B0',
+                image: 'game-controller.jpg'
+            },
+            {status: 'Processing', date: '15/10/2020 14:00', icon: PrimeIcons.COG, color: '#673AB7'},
+            {status: 'Shipped', date: '15/10/2020 16:15', icon: PrimeIcons.ENVELOPE, color: '#FF9800'},
+            {status: 'Delivered', date: '16/10/2020 10:00', icon: PrimeIcons.CHECK, color: '#607D8B'}
+        ];
+    }
 }
