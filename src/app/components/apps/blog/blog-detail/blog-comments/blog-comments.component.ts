9e479139d4104b3edf7797499586912518126560
yigitfindikli <yigitfindikli@gmail.com>
2022-06-23 16:40:22 +0300

    Path fix

--- src/app/components/apps/blog/blog-detail/blog-comments/blog-comments.component.ts
+++ src/app/components/apps/blog/blog-detail/blog-comments/blog-comments.component.ts
@@ -5,7 +5,7 @@ import { Comment } from 'src/app/api/blog';
 @Component({
     selector: 'blog-comments',
     templateUrl: './blog-comments.component.html',
-    styleUrls:['../../bloglist/bloglist.component.scss']
+    styleUrls:['../../blog-list/blog-list.component.scss']
 })
 export class BlogCommentsComponent {
 
