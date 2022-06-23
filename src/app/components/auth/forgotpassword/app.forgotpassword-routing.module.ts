d8472ca9f8dd247355eb723fa94bb0dcb4b1a9c1
Cagatay Civici <cagatay.civici@gmail.com>
2022-06-23 13:27:20 +0300

    Forgot password refactor

--- src/app/components/auth/forgotpassword/app.forgotpassword-routing.module.ts
+++ src/app/components/auth/forgotpassword/app.forgotpassword-routing.module.ts
@@ -3,9 +3,9 @@ import { RouterModule } from '@angular/router';
 import { AppForgotPasswordComponent } from './app.forgotpassword.component';
 
 @NgModule({
-  imports: [RouterModule.forChild([
-    {path: '', component: AppForgotPasswordComponent}
-  ])],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild([
+        { path: '', component: AppForgotPasswordComponent }
+    ])],
+    exports: [RouterModule]
 })
 export class AppForgotPasswordRoutingModule { }
