54442c2d0bb149483c3d5735fb179956eecda176
Cagatay Civici <cagatay.civici@gmail.com>
2022-06-24 11:52:41 +0300

    Refactored FAQ

--- src/app/components/pages/pages-routing.module.ts
+++ src/app/components/pages/pages-routing.module.ts
@@ -7,7 +7,7 @@ import { RouterModule } from '@angular/router';
         { path: 'contact', data: { breadcrumb: 'Contact' }, loadChildren: () => import('./contact/app.contact.module').then(m => m.AppContactModule) },
         { path: 'crud', data: { breadcrumb: 'Crud' }, loadChildren: () => import('./crud/app.crud.module').then(m => m.AppCrudModule) },
         { path: 'empty', data: { breadcrumb: 'Empty' }, loadChildren: () => import('./empty/app.emptydemo.module').then(m => m.AppEmptyDemoModule) },
-        { path: 'faq', data: { breadcrumb: 'FAQ' }, loadChildren: () => import('./faq/app.faq.module').then(m => m.AppFaqModule) },
+        { path: 'faq', data: { breadcrumb: 'FAQ' }, loadChildren: () => import('./faq/faq.module').then(m => m.AppFaqModule) },
         { path: 'help', data: { breadcrumb: 'Help' }, loadChildren: () => import('./help/app.help.module').then(m => m.AppHelpModule) },
         { path: 'invoice', data: { breadcrumb: 'Invoice' }, loadChildren: () => import('./invoice/app.invoice.module').then(m => m.AppInvoiceModule) },
         { path: 'timeline', data: { breadcrumb: 'Timeline' }, loadChildren: () => import('./timeline/app.timelinedemo.module').then(m => m.AppTimelineDemoModule) }
