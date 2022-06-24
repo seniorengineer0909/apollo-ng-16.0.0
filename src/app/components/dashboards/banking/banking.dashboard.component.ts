5836bfd1dde3c1d9133db5419448a7d2a8dc1e8d
yigitfindikli <yigitfindikli@gmail.com>
2022-06-24 17:40:37 +0300

    Error removed from banking dashboard

--- src/app/components/dashboards/banking/banking.dashboard.component.ts
+++ src/app/components/dashboards/banking/banking.dashboard.component.ts
@@ -1,169 +1,166 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 interface File {
-    name: string;
-     image: string;
-     date: string;
-     amount: string;
-     label:string;
+	name: string;
+	image: string;
+	date: string;
+	amount: string;
+	label: string;
 }
 
 @Component({
-    templateUrl: './banking.dashboard.component.html'
+	templateUrl: './banking.dashboard.component.html'
 })
 
-export class BankingDashboardComponent implements OnInit {
-    selectedDate: number;
+export class BankingDashboardComponent {
+	selectedDate: number;
 
-    mounths: any[];
+	mounths: any[];
 
-    selectedMounths: any;
+	selectedMounths: any;
 
-    selectedWeek: any;
+	selectedWeek: any;
 
-    week: any[];
+	week: any[];
 
-    days: any[];
+	days: any[];
 
-    selectedDay: any;
+	selectedDay: any;
 
-    barData = {
-      labels: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
-      datasets: [
-        {
-          label: 'Income',
-          backgroundColor: '#3157DE',
-          borderColor: 'rgb(255, 99, 132)',
-          barThickness: 12,
-          borderRadius: Number.MAX_VALUE,
-          data: [65, 59, 80, 81, 56, 55, 40],
-        },
-        {
-          label: 'Expense',
-          backgroundColor: '#AFC1FF',
-          barThickness: 12,
-          borderColor: 'rgb(54, 162, 235)',
-          borderRadius: Number.MAX_VALUE,
-          data: [28, 48, 40, 19, 86, 27, 90],
-        },
-      ],
-    };
+	barData = {
+		labels: ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+		datasets: [
+			{
+				label: 'Income',
+				backgroundColor: '#3157DE',
+				borderColor: 'rgb(255, 99, 132)',
+				barThickness: 12,
+				borderRadius: Number.MAX_VALUE,
+				data: [65, 59, 80, 81, 56, 55, 40],
+			},
+			{
+				label: 'Expense',
+				backgroundColor: '#AFC1FF',
+				barThickness: 12,
+				borderColor: 'rgb(54, 162, 235)',
+				borderRadius: Number.MAX_VALUE,
+				data: [28, 48, 40, 19, 86, 27, 90],
+			},
+		],
+	};
 
-    barOptions = {
-      plugins: {
-        legend: {
-          labels: {
-            fontColor: '#A0A7B5',
-            usePointStyle: true,
-            font: {
-            weight: 700,
-            },
-            padding: 30,
-          },
-          position: 'bottom',
-        },
-        tooltip: {
-          backgroundColor: '#1F2ED0',
-          titleFont: {
-            weight: 'bold',
-          },
-          bodyFont: {
-          weight: 'bold',
-          },
-          displayColors: false,
-          padding: {
-          left: 40,
-          right: 40,
-          top: 10,
-          bottom: 10,
-        },
-          callbacks: {
-          label: function (tooltipItem) {
-            return `$ ${tooltipItem.raw}`;
-          },
-        },
-      },
-    },
-    scales: {
-      x: {
-        ticks: {
-          font: {
-            weight: 500,
-          },
-        },
-        grid: {
-          display: false,
-          drawBorder: false,
-        },
-      },
-      y: {
-        ticks: {
-          display: false,
-        },
-        grid: {
-          borderDash: [3, 6],
-          color: '#E4EAFF',
-          drawBorder: false,
-          drawTicks: false,
-        },
-      },
-    },
-  };
+	barOptions = {
+		plugins: {
+			legend: {
+				labels: {
+					fontColor: '#A0A7B5',
+					usePointStyle: true,
+					font: {
+						weight: 700,
+					},
+					padding: 30,
+				},
+				position: 'bottom',
+			},
+			tooltip: {
+				backgroundColor: '#1F2ED0',
+				titleFont: {
+					weight: 'bold',
+				},
+				bodyFont: {
+					weight: 'bold',
+				},
+				displayColors: false,
+				padding: {
+					left: 40,
+					right: 40,
+					top: 10,
+					bottom: 10,
+				},
+				callbacks: {
+					label: function (tooltipItem) {
+						return `$ ${tooltipItem.raw}`;
+					},
+				},
+			},
+		},
+		scales: {
+			x: {
+				ticks: {
+					font: {
+						weight: 500,
+					},
+				},
+				grid: {
+					display: false,
+					drawBorder: false,
+				},
+			},
+			y: {
+				ticks: {
+					display: false,
+				},
+				grid: {
+					borderDash: [3, 6],
+					color: '#E4EAFF',
+					drawBorder: false,
+					drawTicks: false,
+				},
+			},
+		},
+	};
 
-  constructor() {
-    this.days = [
-      { name: 'Sunday' },
-      { name: 'Monday' },
-      { name: 'Tuesday' },
-      { name: 'Wednesday' },
-      { name: 'Thursday' },
-      { name: 'Friday' },
-      { name: 'Saturday' },
-    ];
+	constructor() {
+		this.days = [
+			{ name: 'Sunday' },
+			{ name: 'Monday' },
+			{ name: 'Tuesday' },
+			{ name: 'Wednesday' },
+			{ name: 'Thursday' },
+			{ name: 'Friday' },
+			{ name: 'Saturday' },
+		];
 
-    this.week = [{ name: 'Week 1' }, { name: 'Week 2' }];
-  }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+		this.week = [{ name: 'Week 1' }, { name: 'Week 2' }];
+	}
 
-  files: File[] = [
-    {
-        name: "29 Jul 2021",
-        image: "assets/demo/images/avatar/elwinsharvill.png",
-        date: "29 Jul 2021",
-        amount: "$920.34",
-        label: "Complate"
-    },
-    {
-        name: "Courtney Henry",
-        image: "assets/demo/images/avatar/bernardodominic.png",
-        date: "29 Jul 2021",
-        amount: "$834.12",
-        label: "In progress"
-    },
-    {
-        name: "Theresa Webb",
-        image: "assets/demo/images/avatar/ionibowcher.png",
-        date: "29 Jul 2021",
-        amount: "$340.20",
-        label: "Failed"
-    },
-    {
-        name: "Theresa Webb",
-        image: "assets/demo/images/avatar/bernardodominic.png",
-        date: "29 Jul 2021",
-        amount: "$920.34",
-        label: "Complate"
-    }
+	files: File[] = [
+		{
+			name: "29 Jul 2021",
+			image: "assets/demo/images/avatar/elwinsharvill.png",
+			date: "29 Jul 2021",
+			amount: "$920.34",
+			label: "Complate"
+		},
+		{
+			name: "Courtney Henry",
+			image: "assets/demo/images/avatar/bernardodominic.png",
+			date: "29 Jul 2021",
+			amount: "$834.12",
+			label: "In progress"
+		},
+		{
+			name: "Theresa Webb",
+			image: "assets/demo/images/avatar/ionibowcher.png",
+			date: "29 Jul 2021",
+			amount: "$340.20",
+			label: "Failed"
+		},
+		{
+			name: "Theresa Webb",
+			image: "assets/demo/images/avatar/bernardodominic.png",
+			date: "29 Jul 2021",
+			amount: "$920.34",
+			label: "Complate"
+		}
 
-];
-    cols = [
-    { field: 'name', header: 'Name' },
-    { field: 'date', header: 'Date' },
-    { field: 'amount', header: 'Amount' },
-    { field: 'status', header: 'Status' },
-];
+	];
+	cols = [
+		{ field: 'name', header: 'Name' },
+		{ field: 'date', header: 'Date' },
+		{ field: 'amount', header: 'Amount' },
+		{ field: 'status', header: 'Status' },
+	];
 
 
 }
