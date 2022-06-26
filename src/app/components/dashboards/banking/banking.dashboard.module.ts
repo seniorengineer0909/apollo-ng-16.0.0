21a5a19c7b27111d363e6b5637735e80e07b83ac
Cagatay Civici <cagatay.civici@gmail.com>
2022-06-26 13:50:30 +0300

    Refactored banking dashboard

--- src/app/components/dashboards/banking/banking.dashboard.module.ts
+++ src/app/components/dashboards/banking/banking.dashboard.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
-import { DropdownModule } from 'primeng/dropdown';
-import { FormsModule } from '@angular/forms';
 import { TableModule } from 'primeng/table';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { ChartModule } from 'primeng/chart';
 import { RippleModule } from 'primeng/ripple';
+import { TagModule } from 'primeng/tag';
 import { BankingDashboardComponent } from './banking.dashboard.component';
 import { BankingDashboardRoutingModule } from './banking.dashboard-routing.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ButtonModule,
-    RippleModule,
-    DropdownModule,
-    FormsModule,
-    TableModule,
-    InputTextModule,
-    InputTextareaModule,
-    ChartModule,
-    BankingDashboardRoutingModule
-  ],
-  declarations: [BankingDashboardComponent]
+    imports: [
+        CommonModule,
+        ButtonModule,
+        RippleModule,
+        TagModule,
+        TableModule,
+        InputNumberModule,
+        ChartModule,
+        BankingDashboardRoutingModule
+    ],
+    declarations: [BankingDashboardComponent]
 })
 export class BankingDashboardModule { }
