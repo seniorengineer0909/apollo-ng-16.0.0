45b57d909843356b48804fb63cd06fa09855e245
yigitfindikli <yigitfindikli@gmail.com>
2022-06-28 11:08:58 +0300

    Map image added to contact

--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LayoutService } from 'src/app/layout/service/app.layout.service';
 
 @Component({
     templateUrl: './contact.component.html'
@@ -30,4 +31,12 @@ export class ContactComponent {
         {icon: 'pi pi-fw pi-map-marker', title: 'Our Head Office', info:'Churchill-laan 16 II, 1052 CD, Amsterdam'},
         {icon: 'pi pi-fw pi-print', title: 'Fax', info:'3 (833) 297-1548'}
     ];
+
+    constructor(private layoutService: LayoutService) { }
+
+    get mapStyle() {
+        return {
+            'background-image':  this.layoutService.config.colorScheme === 'light' ? "url('/assets/demo/images/contact/map-light.svg')" : "url('/assets/demo/images/contact/map-dark.svg')"
+        }
+    }
 }
