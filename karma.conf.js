10417ae116577f3e58b378089831dd5571fc3fdb
Cagatay Civici <cagatay.civici@gmail.com>
2022-09-20 22:54:02 +0300

    Renamed ngx to ng and set version as 14.0.1

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/apollo-ngx'),
+      dir: require('path').join(__dirname, './coverage/apollo-ng'),
       subdir: '.',
       reporters: [
         { type: 'html' },
