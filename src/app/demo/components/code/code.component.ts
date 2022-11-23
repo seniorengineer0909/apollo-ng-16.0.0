2cdda2fb67c11e14c893a10e8f0e76e58f932d54
Cagatay Civici <cagatay.civici@gmail.com>
2022-11-23 09:57:01 +0300

    Update Kanban Visuals, ECommerce Visuals and Code components

--- src/app/demo/components/code/code.component.ts
+++ src/app/demo/components/code/code.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 @Component({
     selector: 'app-code',
     template: `
-        <pre class="card mb-3 oveflow-auto"><code class="-mt-4 p-0 line-height-3 block" [ngStyle]="{'font-family': 'monaco, Consolas, monospace'}"><ng-content></ng-content></code></pre>
+        <pre class="card mb-3"><code class="-mt-4 p-0 line-height-3 block overflow-auto" [ngStyle]="{'font-family': 'monaco, Consolas, monospace'}"><ng-content></ng-content></code></pre>
     `
 })
 export class AppCodeComponent {
