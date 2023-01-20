c6cd49064bcf7eb8b0320f6e84c67c73c64a7274
gucal <gucalaliriza@outlook.com>
2023-01-20 16:40:28 +0300

    Add reveal menu

--- src/app/layout/app.sidebar.component.ts
+++ src/app/layout/app.sidebar.component.ts
@@ -6,7 +6,31 @@ import { LayoutService } from './service/app.layout.service';
     templateUrl: './app.sidebar.component.html'
 })
 export class AppSidebarComponent {
+    timeout: any = null;
+
 
     constructor(public layoutService: LayoutService, public el: ElementRef) { }
+
+    onMouseEnter() {
+        if (!this.layoutService.state.anchored) {
+            if (this.timeout) {
+                clearTimeout(this.timeout);
+                this.timeout = null;
+            }
+            this.layoutService.state.revealMenuActive = true;
+        }
+    }
+
+    onMouseLeave() {
+        if (!this.layoutService.state.anchored) {
+            if (!this.timeout) {
+                this.timeout = setTimeout(() => this.layoutService.state.revealMenuActive = false, 300);
+            }
+        }
+    }
+
+    anchor() {
+        this.layoutService.state.anchored = !this.layoutService.state.anchored;
+    }
 
 }
